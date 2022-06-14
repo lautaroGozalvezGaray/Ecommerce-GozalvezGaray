18aa4e03b05708b264478f9aa754a6346432efde
Lautaro <lautarogozalvez@gmail.com>
2022-06-14 19:21:53 -0300

    agregando navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,18 @@ import "./NavBar.css";
 
 const NavBar = () =>{
     return(
-        <ul>
-            <li><a className="active" href="#inicio">Inicio</a></li>
-            <li><a href="#productos">Productos</a></li>
-            <li><a href="#nosotros">Nosotros</a></li>
-            <li><a href="#contacto">Contacto</a></li>
-        </ul>
+        <div className="navContainer">
+            <img className="logo" src="https://tonersafe.mx/wp-content/uploads/2019/12/En-construccion.png" alt="logo"></img>
+            <ul>
+                
+                <li><a className="active" href="#inicio">Inicio</a></li>
+                <li><a href="#productos">Productos</a></li>
+                <li><a href="#nosotros">Nosotros</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+
+        </div>
+        
     )
 }
 
